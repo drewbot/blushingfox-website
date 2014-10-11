1c57638eff591ad79ed63871a1603dce01d654bf
Drew Botka <hellodrewbot@gmail.com>
2014-10-11 11:25:49 -0400

    added filter all tip to notify users

--- app/scripts/initialize_Isotope.js
+++ app/scripts/initialize_Isotope.js
@@ -1,12 +1,22 @@
 // Isotope controls
 // Docs: http://isotope.metafizzy.co/
 
+// Hide the filter tip so it can be shown later
+$('.filter-tip').hide();
+
+// Set these variables to false so that when they are later switched to true the ..
+// .. filter tip will not be shown (filter tip is only necessary if user selects "all" first)
+var filterOneClicked = false;
+var filterTwoClicked = false;
+var filterAllClicked = false;
+
 $(document).ready(function() {
 	// Initialize Isotope and set grid container by calling class
 	$('.dynamic-gallery').isotope({
 		// Set items to be placed in grid by calling class
 		itemSelector: '.gallery-image',
-		// Set layout mode to masonry ("pinterest style"): see docs http://isotope.metafizzy.co/layout-modes.html
+		// Set layout mode to masonry ("pinterest style")
+		// see docs http://isotope.metafizzy.co/layout-modes.html
 		layoutMode: 'masonry'
 	});
 });
@@ -15,19 +25,20 @@ $(document).ready(function() {
 // Call button by class for click action to be applied to
 // Filter within a specified container by specified classname
 $('.filter-one-button').click(function(){
+	filterOneClicked = true;
 	$('.dynamic-gallery').isotope({ filter: '.filter-one' })
 })
 
 $('.filter-two-button').click(function(){
+	filterTwoClicked = true;
 	$('.dynamic-gallery').isotope({ filter: '.filter-two' })
 })
 
-var filterAllClicked = false;
-
 $('.filter-all-button').click(function(){
-	if (filterAllClicked == false) {
-		console.log('show all of the photos in the gallery')
-		filterAllClicked = true;
+	if (!filterOneClicked && !filterTwoClicked && !filterAllClicked) {
+		console.log('Filter the gallery by selecting \"Blushing Beauties\" or \"Foxy Ladies\", then select \"all\" to view all again')
+		$('.filter-tip').fadeIn( 400 ).delay( 5000 ).fadeOut( 400 );
 	}	
+	filterAllClicked = true;
 	$('.dynamic-gallery').isotope({ filter: '*' });
 })
