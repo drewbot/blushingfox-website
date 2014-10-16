1b6277e797e99de80395d1291a11b824f4df0265
Drew Botka <hellodrewbot@gmail.com>
2014-10-16 13:44:10 -0400

    working out gh-pages file paths

---
+++ dist/scripts/initialize_Isotope.js
@@ -0,0 +1,43 @@
+// Isotope controls
+// Docs: http://isotope.metafizzy.co/
+
+// Hide the filter tip so it can be shown later
+$('.filter-tip').hide();
+
+// Set these variables to false so that when they are later switched to true the ..
+// .. filter tip will not be shown (filter tip is only necessary if user selects "all" first)
+var filterOneClicked = false;
+var filterTwoClicked = false;
+var filterAllClicked = false;
+
+$(document).ready(function() {
+	// Initialize Isotope and set grid container by calling class
+	$('.dynamic-gallery').isotope({
+		// Set items to be placed in grid by calling class
+		itemSelector: '.gallery-image',
+		// Set layout mode to masonry ("pinterest style")
+		// see docs http://isotope.metafizzy.co/layout-modes.html
+		layoutMode: 'masonry'
+	});
+});
+
+// Filters
+// Call button by class for click action to be applied to
+// Filter within a specified container by specified classname
+$('.filter-one-button').click(function(){
+	filterOneClicked = true;
+	$('.dynamic-gallery').isotope({ filter: '.filter-one' })
+})
+
+$('.filter-two-button').click(function(){
+	filterTwoClicked = true;
+	$('.dynamic-gallery').isotope({ filter: '.filter-two' })
+})
+
+$('.filter-all-button').click(function(){
+	if (!filterOneClicked && !filterTwoClicked && !filterAllClicked) {
+		$('.filter-tip').fadeIn( 400 ).delay( 5000 ).fadeOut( 400 );
+	}	
+	filterAllClicked = true;
+	$('.dynamic-gallery').isotope({ filter: '*' });
+})
---
+++ dist/scripts/main.js
@@ -0,0 +1,24 @@
+
+// Click logo to go to home page
+$('.logo').click(function(){
+	window.location.href = "index.html";
+})
+
+// Click logo to go to home page (from session pages)
+$('.session-logo').click(function(){
+	window.location.href = "../index.html";
+})
+
+// Click menu icon to show nav
+$('.menu-icon').click(function(){
+	$('.nav').addClass('show-nav');
+	$('.menu-icon').addClass('menu-open'); // Hide menu icon
+	$('.menu-x-icon').addClass('menu-x-open'); // Display close icon
+})
+
+// Click menu-x icon to hide nav
+$('.menu-x-icon').click(function(){
+	$('.nav').removeClass('show-nav');
+	$('.menu-icon').removeClass('menu-open'); // Display menu icon
+	$('.menu-x-icon').removeClass('menu-x-open'); // Hide close icon
+})
